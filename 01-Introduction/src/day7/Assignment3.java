03f8e378eb7944c674d70d6c8a69473393ccf6c7
boboaung <boboaung@manjaro>
2022-10-21 20:24:21 +0630

    day7

--- 01-Introduction/src/day7/Assignment3.java
+++ 01-Introduction/src/day7/Assignment3.java
@@ -9,9 +9,61 @@ public class Assignment3 {
 	}
 
 	private static void getUserNRC() {
-		System.out.println("Please input your NRC : ");
 		Scanner sc = new Scanner(System.in);
-		
-		
+		System.out.print("Please input your NRC information : ");
+
+		// input with validate
+		String userNRC = userInputValidate(sc);
+
+		// indexes
+		int townIndex1 = userNRC.indexOf("/");
+		int townIndex2 = userNRC.indexOf("(");
+		int nrcNoIndex1 = userNRC.indexOf(")");
+		int nrcNoIndex2 = userNRC.length();
+		sc.close();
+
+		// output
+		String township = userNRC.substring(townIndex1 + 1, townIndex2);
+		String nrcNo = userNRC.substring(nrcNoIndex1 + 1, nrcNoIndex2);
+		System.out.println("\n----------- Output ----------");
+		System.out.println("Your Township is : " + township.toUpperCase());
+		System.out.println("Your NRC No. is : " + nrcNo);
+		System.out.println("----------- Bye ----------");
+	}
+
+	private static String userInputValidate(Scanner sc) {
+		String userData = sc.nextLine();
+
+		try {
+			while (userData.length() < 17 || userData.length() > 22) {
+				System.out.print("Input NRC information correctly! : ");
+				userData = sc.nextLine();
+
+				while (!userData.contains("/")) {
+					System.out.print("Missing '/', try again! : ");
+					userData = sc.nextLine();
+				}
+				while (!userData.contains("/")) {
+					System.out.print("Missing '/', try again! : ");
+					userData = sc.nextLine();
+				}
+				while (!userData.contains("(")) {
+					System.out.print("Missing '(', try again! : ");
+					userData = sc.nextLine();
+				}
+				while (!userData.contains(")")) {
+					System.out.print("Missing ')', try again! : ");
+					userData = sc.nextLine();
+				}
+				while (!userData.contains("(") || !userData.contains(")")) {
+					System.out.print("Missing '()', try again! : ");
+					userData = sc.nextLine();
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println("Error in user NRC input!");
+		}
+		return userData;
 	}
 }
