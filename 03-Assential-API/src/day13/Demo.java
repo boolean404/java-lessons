6212611c3e99909b479fc06dec62c1e5b5ff1348
boboaung <boboaung@fedora>
2022-11-03 21:17:13 +0630

    day13

---
+++ 03-Assential-API/src/day13/Demo.java
@@ -0,0 +1,31 @@
+package day13;
+
+public class Demo {
+	public static void main(String[] args) {
+		Message msg1 = (str) -> System.out.println("Data : " + str);
+		msg1.display("Hello");
+
+		Authentication auth = (mail, pass) -> {
+			if (mail.equals("admin@gmail.com") && pass.equals("admin"))
+				return true;
+			else
+				return false;
+		};
+
+		boolean result = auth.login("bobo@gmail.com", "admin");
+		if (result)
+			System.err.println("Login Success");
+		else
+			System.out.println("Login Fail!");
+	}
+}
+@FunctionalInterface
+interface Message {
+	void display(String input);
+}
+
+@FunctionalInterface
+interface Authentication {
+	boolean login(String email, String pass);
+}
+
