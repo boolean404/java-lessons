8e6a3f78541743e0a591721fa254c24fdc2c039e
Bo Bo Aung <maungbo404@gmail.com>
2022-11-10 11:13:23 +0630

    day15 SetAssignment

---
+++ 03-Assential-API/src/day15/SetAssignment/Student.java
@@ -0,0 +1,39 @@
+package day15.SetAssignment;
+
+public class Student {
+    private int rollNum;
+    private String name;
+
+
+    public Student(int rollNum, String name) {
+        this.rollNum = rollNum;
+        this.name = name;
+    }
+
+    public Student() {
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNum() {
+        return rollNum;
+    }
+
+    public void setRollNum(int rollNum) {
+        this.rollNum = rollNum;
+    }
+
+    public void insertStudent(int rollNum, String name) {
+        this.rollNum = rollNum;
+        this.name = name;
+    }
+
+}
